c8f9bd6db08d2150cf4c8bb1c6f806cd24112e8a
Sri-Ram-Saran <saran.sridhara@gmail.com>
2024-09-07 15:35:07 +0530

    this is a program in java used to remove duplicates for secound times

---
+++ Dup.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Dup {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        HashMap<Character,Integer> H=new HashMap<>();
+        String s=sc.nextLine();
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+        while(i<s.length()){
+            if(!H.containsKey(s.charAt(i))){
+                H.put(s.charAt(i),1);
+                sb.append(s.charAt(i));
+            }i++;
+            // else{
+            //     H.put(s.charAt(i),1);
+            //     sb.append(s.charAt(i));
+            // }
+        }
+        String d=sb.toString();
+        System.out.println(d);
+        sc.close();
+    }
+}
